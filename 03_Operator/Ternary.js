4b95742174e89219baa8e52fe94765cd14d60501
Harish Korgal <harishkorgal27@gmail.com>
2026-05-19 12:18:16 +0530

    Update Ternary.js

--- 03_Operator/Ternary.js
+++ 03_Operator/Ternary.js
@@ -3,5 +3,5 @@ let result = age > 18 ? "Eligible...🤗" : "Not eligible...🤗";
 console.log(result);
 
 let result1 = false;
-let cond = result1 ? "true condition🙈" : "false condition🎶";
+let cond = result1 ? "true condition...🙈" : "false condition...🎶";
 console.log(cond);
