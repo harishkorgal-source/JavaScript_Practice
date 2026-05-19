4544fb93f25ebec3013d05b2ae60ecf17e8a7db0
Harish Korgal <harishkorgal27@gmail.com>
2026-05-19 12:25:52 +0530

    Update Ternary.js

--- 03_Operator/Ternary.js
+++ 03_Operator/Ternary.js
@@ -5,3 +5,8 @@ console.log(result);
 let result1 = false;
 let cond = result1 ? "true condition...🙈" : "false condition...🎶";
 console.log(cond);
+
+let testResult = "pass";
+let executionResult = "pass";
+let testStatus = executionResult === testResult ? "Pass...🎉" : "Fail...😞";
+console.log(testStatus);
