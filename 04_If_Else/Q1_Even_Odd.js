c3dd2fd0dd53ea8a5829ca05b83335f54eb89a82
Harish Korgal <harishkorgal27@gmail.com>
2026-05-20 17:37:24 +0530

    Add If-Else examples: even/odd checker and marks calculator

---
+++ 04_If_Else/Q1_Even_Odd.js
@@ -0,0 +1,23 @@
+// Even or odd
+let number = 10;
+
+if (number % 2 === 0) {
+    console.log("Even number...");
+} else {
+    console.log("Odd number...");
+}
+
+
+// marks calculator
+let marks = 89;
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 80) {
+    console.log("Grade B");
+} else if (marks >= 70) {
+    console.log("Grade C");
+} else if (marks >= 60) {
+    console.log("Grade B");
+} else {
+    console.log("Fail...");
+}
